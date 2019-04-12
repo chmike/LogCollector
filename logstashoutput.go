c2b50448945c7f896456b1f0bbf8ae0f4b7441e8
Christophe Meessen <meessen@cppm.in2p3.fr>
2019-04-12 09:25:29 +0200

    logstash output tries to reconnect

--- logstashoutput.go
+++ logstashoutput.go
@@ -13,35 +13,37 @@ func logstashOutput(msgs chan []byte, address string) {
 		conn net.Conn
 	)
 	log := l.New(os.Stdout, "logstash", l.Flags())
-
 	for {
-		conn, err = net.Dial("tcp", address)
-		if err == nil {
-			break
+		for {
+			conn, err = net.Dial("tcp", address)
+			if err == nil {
+				break
+			}
+			log.Printf("failed connecting to logstash (%s): %v, wait 10 seconds", address, err)
+			time.Sleep(10 * time.Second)
 		}
-		log.Printf("failed connecting to logstash (%s): %v, wait 10 seconds", address, err)
-		time.Sleep(10 * time.Second)
-	}
-	blob := make([]byte, 0, 4096)
-	ticker := time.NewTicker(flushPeriod)
-	for {
-		select {
-		case msg := <-msgs:
-			// normalize json encoding for logstash (one line json)
-			for i := range msg {
-				if msg[i] == '\n' || msg[i] == '\r' {
-					msg[i] = ' '
+		blob := make([]byte, 0, 4096)
+		ticker := time.NewTicker(flushPeriod)
+		for {
+			select {
+			case msg := <-msgs:
+				// normalize json encoding for logstash (one line json)
+				for i := range msg {
+					if msg[i] == '\n' || msg[i] == '\r' {
+						msg[i] = ' '
+					}
 				}
-			}
-			blob = append(blob, msg...)
-			blob = append(blob, '\n')
+				blob = append(blob, msg...)
+				blob = append(blob, '\n')
 
-		case <-ticker.C:
-			_, err = conn.Write(blob) // may block due to backpressure
-			if err != nil {
-				log.Fatalln("failed forwarding messages to logstash:", err)
+			case <-ticker.C:
+				_, err = conn.Write(blob) // may block due to backpressure
+				if err != nil {
+					log.Println("failed forwarding messages to logstash:", err)
+					break
+				}
+				blob = blob[:0]
 			}
-			blob = blob[:0]
 		}
 	}
 }
